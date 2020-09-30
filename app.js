cd4c29262246782872616e238edb29da450fa148
DickensJuma <dickensjuma13@gmail.com>
2020-09-30 18:52:10 +0300

    express

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -43,25 +43,10 @@ app.use(cookieParser());
 
 
 app.use('/', indexRouter);
+const port = 3000
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-res.json({
-  message: err.message,
-  error: err
-});
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 module.exports = app;
